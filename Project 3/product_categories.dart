873549b94a3dfd3628be5c907bab6f13c0ac74df
Aya Mohamed <163604760+Aya-Mohamed80@users.noreply.github.com>
2025-07-26 20:15:06 +0300

    Create product_categories.dart

---
+++ Project 3/product_categories.dart
@@ -0,0 +1,21 @@
+ import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> categories = [
+    {'icon': Icons.chair, 'label': 'Chairs'},
+    {'icon': Icons.bed, 'label': 'Beds'},
+    {'icon': Icons.table_bar, 'label': 'Tables'},
+    {'icon': Icons.chair_alt, 'label': 'Sofas'},
+    {'icon': Icons.kitchen, 'label': 'Kitchens'},
+    {'icon': Icons.storage, 'label': 'Storage'},
+    {'icon': Icons.wallpaper, 'label': 'Decor'},
+    {'icon': Icons.lightbulb, 'label': 'Lighting'},
+    {'icon': Icons.plumbing, 'label': 'Plumbing'},
+  ];
+
+  final List<Map<String, dynamic>> products = [
+    {
+      'name': 'Luxury Sofa',
+      'image': 'assets/sofa.jpg',
+      'price': '\$499',
+    },
+  ];
