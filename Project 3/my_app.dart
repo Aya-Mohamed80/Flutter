35359053c0870de88d48c6ad937425dd89bab553
Aya Mohamed <163604760+Aya-Mohamed80@users.noreply.github.com>
2025-07-26 19:15:44 +0300

    Create my_app.dart

---
+++ Project 3/my_app.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:iti_flutter/home_screen.dart';
+
+class FurnitureStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
