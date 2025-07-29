679ae5ae28d228eac0deb88081cdd7b05958d253
Aya Mohamed <163604760+Aya-Mohamed80@users.noreply.github.com>
2025-07-29 17:05:21 +0300

    Add files via upload

---
+++ firebase_auth/lib/features/login/screen/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:iti_project/features/login/screen/login_screen_conent.dart';
+import 'package:iti_project/features/login/bloc/login_bloc.dart';
+import 'package:iti_project/features/login/auth_repo_login.dart';
+import 'package:iti_project/core/app_colors.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => LoginBloc(authLoginRepo: FirebaseAuthLoginService()),
+      child: Scaffold(
+        backgroundColor: AppColors.white,
+        body: SafeArea(child: LoginScreenContent()),
+      ),
+    );
+  }
+}
